12084535514e011426379b65ea1693475c7cc83d
kzajaczkowski <krzysztof.lukasz.zajaczkowski@gmail.com>
2024-04-24 18:10:15 +0200

    Experimenting

--- dbt/adapters/mysql/connections.py
+++ dbt/adapters/mysql/connections.py
@@ -106,6 +106,7 @@ class MySQLConnectionManager(SQLConnectionManager):
                 else:
                     connection.handle.set_charset_collation(credentials.charset)
             connection.state = "open"
+            connection.handle.set_charset_collation("utf8mb4", "utf8mb4_0900_ai_ci")
         except mysql.connector.Error:
             try:
                 logger.debug(
